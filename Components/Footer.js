46aa01aa9270d06c332a3d890d6d715fc36da2dd
areeshaak30 <areesha.ak30@gmail.com>
2024-05-22 18:01:06 +0500

    Footer_Changings_Part_01

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -10,6 +10,16 @@ import Svg, { Path } from "react-native-svg";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function Footer() {
+  const [buttonPressed, setButtonPressed] = useState(false);
+
+  const handlePressIn = () => {
+    setButtonPressed(true);
+  };
+
+  const handlePressOut = () => {
+    setButtonPressed(false);
+  };
+
   return (
     <View style={styles.footerContainer}>
       <View style={styles.footerContent}>
@@ -18,7 +28,7 @@ export default function Footer() {
             style={{
               flex: 1,
               flexDirection: "column",
-              alignItems: "start",
+              alignItems: "flex-start",
               backgroundColor: "#0f625c",
               paddingLeft: 10,
               paddingTop: 10,
@@ -38,27 +48,27 @@ export default function Footer() {
               Scrapify
             </Text>
             <View style={styles.addressContainer}>
-              <Svg width={19} height={19} viewBox="0 0 16 16">
+              <Svg width={14} height={14} viewBox="0 0 16 16">
                 <Path
-                  fill="#52011A"
+                  fill="#FFC208"
                   d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"
                 />
               </Svg>
               <Text style={styles.addressText}>35 Main Nisbat Road Lahore</Text>
             </View>
             <View style={styles.addressContainer}>
-              <Svg width={19} height={19} viewBox="0 0 16 16">
+              <Svg width={14} height={14} viewBox="0 0 16 16">
                 <Path
-                  fill="#52011A"
+                  fill="#FFC208"
                   d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"
                 />
               </Svg>
               <Text style={styles.addressText}>abcxyzjh@gmail.com</Text>
             </View>
             <View style={styles.addressContainer}>
-              <Svg width={19} height={19} viewBox="0 0 16 16">
+              <Svg width={14} height={14} viewBox="0 0 16 16">
                 <Path
-                  fill="#52011A"
+                  fill="#FFC208"
                   d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
                 />
               </Svg>
@@ -70,9 +80,11 @@ export default function Footer() {
           <View style={{ paddingLeft: 50 }}>
             <Text style={styles.footerHeading}>Quick Link</Text>
             <View>
-              <Text>Home</Text>
-              <Text>About</Text>
-              <Text>Contact</Text>
+              <Text style={{color: "white"}}>Home</Text>
+              <Text style={{color: "white"}}>About Us</Text>
+              <Text style={{color: "white"}}>Services</Text>
+              <Text style={{color: "white"}}>Projects</Text>
+              <Text style={{color: "white"}}>Contact Us</Text>
             </View>
           </View>
         </View>
@@ -84,24 +96,41 @@ export default function Footer() {
               placeholder="Your Email Address"
               placeholderTextColor="grey"
             />
-            <TouchableOpacity style={styles.subscribeButton}>
-              <Text style={styles.subscribeButtonText}>Subscribe</Text>
+            <TouchableOpacity
+              style={[
+                styles.subscribeButton,
+                buttonPressed ? styles.subscribeButtonPressed : null,
+              ]}
+              onPressIn={handlePressIn}
+              onPressOut={handlePressOut}
+            >
+              <Text
+                style={
+                  buttonPressed
+                    ? styles.subscribeButtonTextPressed
+                    : styles.subscribeButtonText
+                }
+              >
+                Subscribe
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
       </View>
-      <View style={{flex:1, flexDirection:"column", alignItems:'center'}}>
-        <Text style={{color:"#cacaca", paddingTop:10}}>Get Latest Updates at: </Text>
+      <View style={{ flex: 1, flexDirection: "column", alignItems: "center" }}>
+        <Text style={{ color: "#cacaca", paddingTop: 10 }}>
+          Get Latest Updates at:{" "}
+        </Text>
         <View style={styles.socialIcons}>
-        <Icon name="facebook" size={30} color="#3b5998" />
-        <Icon name="twitter" size={30} color="#1da1f2" />
-        <Icon name="instagram" size={30} color="#bc2a8d" />
+          <Icon name="facebook" size={30} color="#3b5998" />
+          <Icon name="twitter" size={30} color="#1da1f2" />
+          <Icon name="instagram" size={30} color="#bc2a8d" />
         </View>
       </View>
     </View>
   );
 }
-// -- STYLING --
+
 const styles = StyleSheet.create({
   footerContainer: {
     backgroundColor: "#088178",
@@ -132,6 +161,8 @@ const styles = StyleSheet.create({
     fontSize: 12,
     lineHeight: 16,
     color: "#cacaca",
+    flexWrap: "wrap",
+    flexShrink: 1,
   },
   input: {
     borderWidth: 1,
@@ -151,15 +182,21 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     width: "100%",
   },
+  subscribeButtonPressed: {
+    backgroundColor: "#FFC208",
+  },
   subscribeButtonText: {
     color: "#088178",
+  },
+  subscribeButtonTextPressed: {
+    color: "#000",
   },
   newsletterContainer: {
     width: "100%",
   },
   socialIcons: {
     flexDirection: "row",
-    justifyContent:"center",
+    justifyContent: "center",
     marginTop: 20,
     gap: 10,
   },
--- pages/SingleProduct/SingleProductDetailScreen.js
+++ pages/SingleProduct/SingleProductDetailScreen.js
@@ -1,364 +1,106 @@
-// import { useRoute } from "@react-navigation/native";
-// import React, { useState, useEffect, useRef } from "react";
-// import { View, Text, Image, TouchableOpacity, FlatList } from "react-native";
-// import { styles } from "./style.module";
-// import ThumbnailList from "./ThumbnailList";
-
-// import prod01Full from "../../assets/img/prod-01-full.jpg";
-// import prod01Full02 from "../../assets/img/prod-01-full(02).jpg";
-// import prod01Full03 from "../../assets/img/prod-01-full(03).jpg";
-// import prod01Full04 from "../../assets/img/prod-01-full(04).jpg";
-// import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-// import { faStar, faCartPlus } from "@fortawesome/free-solid-svg-icons";
-// import YouMayLike from "../../Components/Product_Listings/YouMayLike";
-
-// const SingleProductDetailScreen = ({ navigation }) => {
-//   const route = useRoute();
-//   const { data } = route.params;
-//   const [mainImage, setMainImage] = useState(data.imgSrc);
-//   const [quantity, setQuantity] = useState(1);
-//   const flatListRef = useRef(null);
-
-//   const handleThumbnailClick = (image) => {
-//     setMainImage(image);
-//   };
-
-//   const handleIncreaseQuantity = () => {
-//     setQuantity(quantity + 1);
-//   };
-
-//   const handleDecreaseQuantity = () => {
-//     if (quantity > 1) {
-//       setQuantity(quantity - 1);
-//     }
-//   };
-
-//   useEffect(() => {
-//     flatListRef.current.scrollToOffset({ offset: 0, animated: true });
-//     setMainImage(data.imgSrc);
-//   }, [data]);
-
-//   const thumbnailImages = [
-//     data.imgSrc,
-//     prod01Full,
-//     prod01Full02,
-//     prod01Full03,
-//     prod01Full04,
-//   ];
-
-//   const redirectToProductDetail = (product) => {
-//     navigation.navigate("SingleProduct", { data: product });
-//   };
-
-//   return (
-//     <FlatList
-//       ref={flatListRef}
-//       data={[{ key: "mainView" }]}
-//       renderItem={({ item }) => (
-//         <View style={styles.container}>
-//           <View style={styles.productDetailsContainer}>
-//             <View style={styles.mainImageContainer}>
-//               <Image source={mainImage} style={styles.mainImage} />
-//             </View>
-
-//             <ThumbnailList
-//               thumbnailImages={thumbnailImages}
-//               onThumbnailClick={handleThumbnailClick}
-//             />
-//             <Text style={styles.productName}>ELLEGANCE FLORA - GREEN</Text>
-//             <Text style={styles.productPrice}>PKR 9,845.00</Text>
-//             <View style={styles.sizeOptionsContainer}>
-//               <TouchableOpacity style={styles.sizeOption}>
-//                 <Text style={styles.sizeOptionText}>S</Text>
-//               </TouchableOpacity>
-//             </View>
-//             <View style={styles.quantityContainer}>
-//               <TouchableOpacity onPress={handleDecreaseQuantity}>
-//                 <Text style={styles.quantityButton}>-</Text>
-//               </TouchableOpacity>
-//               <Text style={styles.quantityButton}>{quantity}</Text>
-//               <TouchableOpacity onPress={handleIncreaseQuantity}>
-//                 <Text style={styles.quantityButton}>+</Text>
-//               </TouchableOpacity>
-//             </View>
-//             <TouchableOpacity style={styles.addToCartButton}>
-//               <Text style={styles.addToCartButtonText}>Add to Cart</Text>
-//             </TouchableOpacity>
-//             <Text style={styles.productDescription}>Product Details:</Text>
-//             <Text style={styles.productDetailsText}>
-//               Ellegance Flora {"\n"}
-//               <Text style={{ fontWeight: "bold" }}>Shirt:</Text> Embroidered
-//               Cotton Karandi Sp 2.50 Meter {"\n"}
-//               <Text style={{ fontWeight: "bold" }}>Dupatta:</Text> Embroidered
-//               Chiffon Self 2.50 Meter {"\n"}
-//               <Text style={{ fontWeight: "bold" }}>Shalwar:</Text> Plain Cotton
-//               Karandi Sp 2.50 Meter
-//             </Text>
-//           </View>
-//           <YouMayLike navigation={navigation} />
-//         </View>
-//       )}
-//       keyExtractor={(item) => item.key}
-//     />
-//   );
-// };
-
-// export default SingleProductDetailScreen;
-
-
-
-
-import React, { useState, useEffect, useRef } from 'react';
-import {
-    StyleSheet, Button, View, StatusBar, ToastAndroid, Dimensions, Image, TouchableOpacity,
-    FlatList, Animated, Easing, Text
-} from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRoute } from "@react-navigation/native";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faStar, faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import React, { useState, useEffect, useRef } from "react";
+import { View, Text, Image, TouchableOpacity, FlatList } from "react-native";
+import { styles } from "./style.module";
 import ThumbnailList from "./ThumbnailList";
-import YouMayLike from "../../Components/Product_Listings/YouMayLike";
 
 import prod01Full from "../../assets/img/prod-01-full.jpg";
 import prod01Full02 from "../../assets/img/prod-01-full(02).jpg";
 import prod01Full03 from "../../assets/img/prod-01-full(03).jpg";
 import prod01Full04 from "../../assets/img/prod-01-full(04).jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faStar, faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import YouMayLike from "../../Components/Product_Listings/YouMayLike";
 
 const SingleProductDetailScreen = ({ navigation }) => {
-    const route = useRoute();
-    const { data } = route.params;
+  const route = useRoute();
+  const { data } = route.params;
+  const [mainImage, setMainImage] = useState(data.imgSrc);
+  const [quantity, setQuantity] = useState(1);
+  const flatListRef = useRef(null);
 
-    const [mainImage, setMainImage] = useState(data.imgSrc);
-    const [quantity, setQuantity] = useState(1);
-    const [products, setProducts] = useState([]);
-    const flatListRef = useRef(null);
-    const width = Dimensions.get('window').width;
-    const scrollX = new Animated.Value(0);
-    const position = Animated.divide(scrollX, width);
+  const handleThumbnailClick = (image) => {
+    setMainImage(image);
+  };
 
-    useEffect(() => {
-        flatListRef.current.scrollToOffset({ offset: 0, animated: true });
-        setMainImage(data.imgSrc);
-        const unsubscribe = navigation.addListener('focus', () => {
-            loadProducts();
-        });
-        return unsubscribe;
-    }, [data, navigation]);
+  const handleIncreaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
 
-    const loadProducts = async () => {
-        let values = await AsyncStorage.getItem('products');
-        values = JSON.parse(values);
-        setProducts(values || []);
-    };
+  const handleDecreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
 
-    const handleThumbnailClick = (image) => {
-        setMainImage(image);
-    };
+  useEffect(() => {
+    flatListRef.current.scrollToOffset({ offset: 0, animated: true });
+    setMainImage(data.imgSrc);
+  }, [data]);
 
-    const handleIncreaseQuantity = () => {
-        setQuantity(quantity + 1);
-    };
+  const thumbnailImages = [
+    data.imgSrc,
+    prod01Full,
+    prod01Full02,
+    prod01Full03,
+    prod01Full04,
+  ];
 
-    const handleDecreaseQuantity = () => {
-        if (quantity > 1) {
-            setQuantity(quantity - 1);
-        }
-    };
+  const redirectToProductDetail = (product) => {
+    navigation.navigate("SingleProduct", { data: product });
+  };
 
-    const addToCart = async (product) => {
-        let cartItems = await AsyncStorage.getItem('cartItems');
-        cartItems = JSON.parse(cartItems) || [];
-        
-        const existingItem = cartItems.find(item => item.id === product.id);
-        if (existingItem) {
-            existingItem.quantity += 1;
-            existingItem.productPrice += product.productPrice;
-        } else {
-            cartItems.push({ ...product, quantity: 1 });
-        }
+  return (
+    <FlatList
+      ref={flatListRef}
+      data={[{ key: "mainView" }]}
+      renderItem={({ item }) => (
+        <View style={styles.container}>
+          <View style={styles.productDetailsContainer}>
+            <View style={styles.mainImageContainer}>
+              <Image source={mainImage} style={styles.mainImage} />
+            </View>
 
-        try {
-            await AsyncStorage.setItem('cartItems', JSON.stringify(cartItems));
-            ToastAndroid.show('Item Added Successfully to cart', ToastAndroid.SHORT);
-            navigation.navigate('Home');
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    const thumbnailImages = [
-        data.imgSrc,
-        prod01Full,
-        prod01Full02,
-        prod01Full03,
-        prod01Full04,
-    ];
-
-    return (
-        <FlatList
-            ref={flatListRef}
-            data={[{ key: "mainView" }]}
-            renderItem={() => (
-                <View style={styles.container}>
-                    <View style={styles.productDetailsContainer}>
-                        <FlatList
-                            data={products.productImageList}
-                            horizontal
-                            renderItem={({ item }) => (
-                                <View style={styles.imageContainer}>
-                                    <Image source={item} style={styles.mainImage} />
-                                </View>
-                            )}
-                            showsHorizontalScrollIndicator={false}
-                            decelerationRate={0.8}
-                            snapToInterval={width}
-                            bounces={false}
-                            onScroll={Animated.event(
-                                [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-                                { useNativeDriver: false },
-                            )}
-                        />
-                        <View style={styles.indicatorContainer}>
-                            {products.productImageList && products.productImageList.map((_, index) => {
-                                let opacity = position.interpolate({
-                                    inputRange: [index - 1, index, index + 1],
-                                    outputRange: [0.2, 1, 0.2],
-                                    extrapolate: 'clamp',
-                                });
-                                return (
-                                    <Animated.View
-                                        key={index}
-                                        style={{ ...styles.indicator, opacity }}
-                                    />
-                                );
-                            })}
-                        </View>
-
-                        <ThumbnailList
-                            thumbnailImages={thumbnailImages}
-                            onThumbnailClick={handleThumbnailClick}
-                        />
-
-                        <Text style={styles.productName}>{products.productName}</Text>
-                        <Text style={styles.productPrice}>&#x20b9; {products.productPrice}</Text>
-
-                        <View style={styles.quantityContainer}>
-                            <TouchableOpacity onPress={handleDecreaseQuantity}>
-                                <Text style={styles.quantityButton}>-</Text>
-                            </TouchableOpacity>
-                            <Text style={styles.quantityButton}>{quantity}</Text>
-                            <TouchableOpacity onPress={handleIncreaseQuantity}>
-                                <Text style={styles.quantityButton}>+</Text>
-                            </TouchableOpacity>
-                        </View>
-
-                        <TouchableOpacity
-                            onPress={() => addToCart(products)}
-                            style={styles.addToCartButton}>
-                            <Text style={styles.addToCartButtonText}>
-                                {products.isAvailable ? 'Add to cart' : 'Not Available'}
-                            </Text>
-                        </TouchableOpacity>
-
-                        <Text style={styles.productDescription}>Product Details:</Text>
-                        <Text style={styles.productDetailsText}>{products.description}</Text>
-                    </View>
-                    <YouMayLike navigation={navigation} />
-                </View>
-            )}
-            keyExtractor={(item) => item.key}
-        />
-    );
+            <ThumbnailList
+              thumbnailImages={thumbnailImages}
+              onThumbnailClick={handleThumbnailClick}
+            />
+            <Text style={styles.productName}>ELLEGANCE FLORA - GREEN</Text>
+            <Text style={styles.productPrice}>PKR 9,845.00</Text>
+            <View style={styles.sizeOptionsContainer}>
+              <TouchableOpacity style={styles.sizeOption}>
+                <Text style={styles.sizeOptionText}>S</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.quantityContainer}>
+              <TouchableOpacity onPress={handleDecreaseQuantity}>
+                <Text style={styles.quantityButton}>-</Text>
+              </TouchableOpacity>
+              <Text style={styles.quantityButton}>{quantity}</Text>
+              <TouchableOpacity onPress={handleIncreaseQuantity}>
+                <Text style={styles.quantityButton}>+</Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.addToCartButton}>
+              <Text style={styles.addToCartButtonText}>Add to Cart</Text>
+            </TouchableOpacity>
+            <Text style={styles.productDescription}>Product Details:</Text>
+            <Text style={styles.productDetailsText}>
+              Ellegance Flora {"\n"}
+              <Text style={{ fontWeight: "bold" }}>Shirt:</Text> Embroidered
+              Cotton Karandi Sp 2.50 Meter {"\n"}
+              <Text style={{ fontWeight: "bold" }}>Dupatta:</Text> Embroidered
+              Chiffon Self 2.50 Meter {"\n"}
+              <Text style={{ fontWeight: "bold" }}>Shalwar:</Text> Plain Cotton
+              Karandi Sp 2.50 Meter
+            </Text>
+          </View>
+          <YouMayLike navigation={navigation} />
+        </View>
+      )}
+      keyExtractor={(item) => item.key}
+    />
+  );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#EFF1F0',
-    },
-    productDetailsContainer: {
-        padding: 20,
-    },
-    mainImageContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: 300,
-    },
-    mainImage: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'contain',
-    },
-    imageContainer: {
-        width: Dimensions.get('window').width,
-        height: 300,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    indicatorContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginBottom: 16,
-        marginTop: 32,
-    },
-    indicator: {
-        width: '16%',
-        height: 2.4,
-        backgroundColor: 'black',
-        marginHorizontal: 4,
-        borderRadius: 100,
-    },
-    productName: {
-        fontSize: 30,
-        color: 'black',
-        marginVertical: 10,
-    },
-    productPrice: {
-        fontSize: 30,
-        color: 'black',
-        marginVertical: 10,
-    },
-    quantityContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginVertical: 10,
-    },
-    quantityButton: {
-        fontSize: 20,
-        fontWeight: '500',
-        letterSpacing: 1,
-        color: '#000',
-        marginHorizontal: 10,
-    },
-    addToCartButton: {
-        width: '86%',
-        height: 50,
-        backgroundColor: 'blue',
-        borderRadius: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginVertical: 30,
-    },
-    addToCartButtonText: {
-        fontSize: 20,
-        fontWeight: '500',
-        letterSpacing: 1,
-        color: '#fff',
-        textTransform: 'uppercase',
-    },
-    productDescription: {
-        fontSize: 20,
-        color: 'black',
-        marginVertical: 10,
-    },
-    productDetailsText: {
-        fontSize: 15,
-        color: '#777A76',
-    },
-});
 
 export default SingleProductDetailScreen;
